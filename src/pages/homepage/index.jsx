f4d207cbfc8f0129da2132647d988a2f238b544a
knightSarai <knight.sarai.dev@gmail.com>
2020-10-06 19:49:14 +0300

    homepage sass file

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './homepage.styles.scss'
 
 // import directory from '../../components/directory/directory.component'
 
 export default function hompage() {
     return (
-        <div className="hompage">
+        <div className="homepage container">
             <h1>homepage</h1>
         </div>
     )
