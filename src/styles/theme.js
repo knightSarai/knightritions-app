ada9ca40d7768457c112974422fd44f6d647050f
knightSarai <knight.sarai.dev@gmail.com>
2020-10-06 19:54:10 +0300

    max width: 1200

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -19,7 +19,7 @@ export const theme = new (function() {
         products: "#EF5D13"
     }
     this.size = {
-        maxWidth: "1100px",
+        maxWidth: "1200px",
         mobile: '625px'
     }
 })();
