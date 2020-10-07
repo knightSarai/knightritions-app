2b6b29fc972688191d3ae5953598857872210df8
knightSarai <knight.sarai.dev@gmail.com>
2020-10-07 18:49:48 +0300

    rename to shop.data

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,127 @@
+export default {
+    collections: [
+        {
+            id: 1,
+            title: "Supplements",
+            routeName: "supplements",
+            items: [
+                {
+                    id: 1,
+                    name: "Dymatize iso",
+                    img: '/img/shop/supplements/dymatize-iso/main.png'
+                },
+                {
+                    id: 2,
+                    name: "Cereatine",
+                    img: '/img/shop/supplements/cereatine/main.png'
+                },
+                {
+                    id: 3,
+                    name: "Pre workouts",
+                    img: '/img/shop/supplements/pre-workouts/main.png'
+                },
+                {
+                    id: 4,
+                    name: "BCCA's",
+                    img: '/img/shop/supplements/bcca/main.png'
+                },
+                {
+                    id: 5,
+                    name: "Amplifir",
+                    img: '/img/shop/supplements/amplifir/main.png'
+                }
+            ]
+        },
+        {
+            id: 2,
+            title: "Products",
+            routeName: "products",
+            items: [
+                {
+                    id: 6,
+                    name: "Heavy Bags",
+                    img: '/img/shop/products/bags/main.png'
+                },
+                {
+                    id: 7,
+                    name: "Boxing gloves",
+                    img: '/img/shop/products/gloves/main.png'
+                },
+                {
+                    id: 8,
+                    name: "Paralletes",
+                    img: '/img/shop/products/paralletes/main.png'
+                },
+                {
+                    id: 9,
+                    name: "Rings",
+                    img: '/img/shop/products/rings/main.png'
+                },
+                {
+                    id: 10,
+                    name: "Wrest Wraps",
+                    img: '/img/shop/products/wrest-wraps/main.png'
+                },
+            ]
+        },
+        {
+            id: 3,
+            title: "Fitness Clothing",
+            routeName: "clothing",
+            items: [
+                {
+                    id: 11,
+                    name: "Pants",
+                    img: '/img/shop/fitness-clothing/pants/main.png'
+                },
+                {
+                    id: 12,
+                    name: "Shoes",
+                    img: '/img/shop/fitness-clothing/shoes/main.png'
+                },
+                {
+                    id: 13,
+                    name: "Shorts",
+                    img: '/img/shop/fitness-clothing/shorts/main.png'
+                },
+                {
+                    id: 14,
+                    name: "T Shirts",
+                    img: '/img/shop/fitness-clothing/t-shirts/main.png'
+                },
+            ]
+        },
+        {
+            id: 4,
+            title: "Home Gym",
+            routeName: "homegym",
+            items: [
+                {
+                    id: 15,
+                    name: "Benchs",
+                    img: '/img/shop/home-gym/Benchs/main.png'
+                },
+                {
+                    id: 16,
+                    name: "Mats",
+                    img: '/img/shop/home-gym/mats/main.png'
+                },
+                {
+                    id: 17,
+                    name: "PullUp Bars",
+                    img: '/img/shop/home-gym/pullup-bar/main.png'
+                },
+                {
+                    id: 18,
+                    name: "PullUp Bars",
+                    img: '/img/shop/home-gym/treadmill/main.png'
+                },
+                {
+                    id: 19,
+                    name: "Weights",
+                    img: '/img/shop/home-gym/weights/main.png'
+                }
+            ]
+        }
+    ]
+}
