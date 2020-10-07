3dfa2b1edfa4978e17bf48e39cccebf9af7620ae
knightSarai <knight.sarai.dev@gmail.com>
2020-10-07 18:20:11 +0300

    add shop imgs

---
+++ src/pages/shop/shopData.js
@@ -0,0 +1,36 @@
+export default {
+    collections: [
+        {
+            id: 1,
+            title: "Supplements",
+            routeName: "supplements",
+            items: [
+
+            ]
+        },
+        {
+            id: 2,
+            title: "Fitness Clothing",
+            routeName: "clothing",
+            items: [
+                
+            ]
+        },
+        {
+            id: 3,
+            title: "Products",
+            routeName: "products",
+            items: [
+                
+            ]
+        },
+        {
+            id: 4,
+            title: "Home Gym",
+            routeName: "homegym",
+            items: [
+                
+            ]
+        }
+    ]
+}
