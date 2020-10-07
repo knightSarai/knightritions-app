63f2cc67ad423e4baa25fd07499db57a564a9ffe
knightSarai <knight.sarai.dev@gmail.com>
2020-10-07 16:09:18 +0300

    edit directory data urls, add match.url add shop.jsx, add shop data

--- src/components/directory/dirctoryData.js
+++ src/components/directory/dirctoryData.js
@@ -3,25 +3,25 @@ export default  {
         {
             title: 'Supplements',
             imgUrl: '/img/directory/supplements.jpg',
-            linkUrl: '/supplements',
+            linkUrl: 'supplements',
             id: 1
         },
         {
             title: 'Fitness clothing',
             imgUrl: '/img/directory/clothes.jpg',
-            linkUrl: '/clothing',
+            linkUrl: 'clothing',
             id: 2
         },
         {
             title: 'Products',
             imgUrl: '/img/directory/products.jpg',
-            linkUrl: '/products',
+            linkUrl: 'products',
             id: 3
         },
         {
             title: 'Home Gym',
             imgUrl: '/img/directory/homegym.jpg',
-            linkUrl: '/homegyme',
+            linkUrl: 'homegyme',
             id: 4
         },
     ]
--- src/components/menu-item/index.jsx
+++ src/components/menu-item/index.jsx
@@ -3,10 +3,8 @@ import {withRouter} from 'react-router-dom'
 import {StyledDirectory} from './menu-item.styles';
 
 function menuItem({title, imgUrl, history, match, linkUrl}) {
-    console.log(imgUrl);
-    console.log(match);
     return (
-        <StyledDirectory img={imgUrl} onClick={() => history.push(`${linkUrl}`)}>
+        <StyledDirectory img={imgUrl} onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div className="background-img"/>
             <div className="content">
                 <h1 className="item-title">{title}</h1>
