8a1741c34c0bd25adee213159d462f081a266c44
knightSarai <knight.sarai.dev@gmail.com>
2020-11-02 13:10:29 +0200

    cart redux

--- src/components/App-bar/cart/cart.jsx
+++ src/components/App-bar/cart/cart.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import {toggleCartHidden} from '../../../redux/cart/cart.action'
+
 import CartIcon from './cart.styles';
 import {HiOutlineShoppingBag} from 'react-icons/hi';
 
-export default function Cart() {
+function Cart({toggleCartHidden}) {
     return (
-        <CartIcon>
+        <CartIcon onClick={toggleCartHidden}>
             <HiOutlineShoppingBag className="shopping-icon"/>
             <span className="item-count">0</span>
         </CartIcon>
     )
 }
+
+
+
+export default connect(null, {toggleCartHidden})(Cart)
