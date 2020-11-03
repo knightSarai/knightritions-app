529739238e1cabc875dd56e89b141dd53b0ab86d
knightSarai <knight.sarai.dev@gmail.com>
2020-11-03 21:04:16 +0200

    remove item from cart

--- src/redux/cart/cart.util.js
+++ src/redux/cart/cart.util.js
@@ -8,6 +8,19 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
             : cartItem
         )
     }
-
     return [...cartItems, {...cartItemsToAdd, quantity: 1}]
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+
+    if (existItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map(cartItem => 
+        cartItem.id === cartItemToRemove.id
+        ? {...cartItem, quantity: cartItem.quantity - 1}
+        : cartItem
+    )
 }
