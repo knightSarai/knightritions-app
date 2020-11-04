4a06d381fb73ec280c4d1c4748d66ce1f7cbbb82
knightSarai <knight.sarai.dev@gmail.com>
2020-11-04 08:13:41 +0200

    add persist to app

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/** Routers */
 import {BrowserRouter as Router} from 'react-router-dom';
+/** Redux */
 import {Provider as ReduxProvider} from 'react-redux';
-import store from './redux/store'
-
+import {PersistGate} from 'redux-persist/integration/react';
+import {store, persistor} from './redux/store';
+/**Components */
 import App from './App';
-
+/*Styles*/
 import './index.css';
-
-
-
 
 ReactDOM.render(
   <ReduxProvider store={store}>
     <Router>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Router>
   </ReduxProvider>,
   document.getElementById('root')
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import {createStore, compose} from 'redux';
 import reducer from './root.reducer';
+import { persistStore } from 'redux-persist';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose;
 
-const store = createStore(
+export const store = createStore(
     reducer, 
     composeEnhancers()
-    )
+);
+
+export const persistor = persistStore(store);
 
 export default store;
