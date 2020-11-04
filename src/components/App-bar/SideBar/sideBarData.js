8a9a784c60b7a41130a6ff3ca9f04a245b24e2f8
knightSarai <knight.sarai.dev@gmail.com>
2020-11-04 09:05:06 +0200

    clear Icons

--- src/components/App-bar/SideBar/sideBarData.js
+++ src/components/App-bar/SideBar/sideBarData.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
-
 export const sideBareData = [
     {
         title: "Home",
         path: "/",
-        icon: <AiIcons.AiFillHome/>,
     },
     {
         title: "Blog",
         path: "/blog",
-        icon: <IoIcons.IoIosPaper/>,
     },
     {
         title: "Contact",
         path: "/contact",
-        icon: <IoIcons.IoIosPaper/>,
     },
 ];
 
