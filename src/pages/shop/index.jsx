f756a746a47caf6e0ffc0beeea31f7d3d6155579
knightSarai <knight.sarai.dev@gmail.com>
2020-11-04 10:24:48 +0200

    add shop data to redux

--- src/pages/shop/index.jsx
+++ src/pages/shop/index.jsx
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+import {selectShopItems} from '../../redux/shop/shop.selector'
 import {v4 as uuid} from 'uuid';
-import shopData from './shop.data';
 import ShopCollection from '../../components/shop-collection';
 import './shop.styles.scss';
 
-export default class ShopPage extends Component {
-    state = {
-        ...shopData
-    }
-    
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page container">
-                {
-                    collections.map(collection => (
-                        <ShopCollection key={uuid()} {...collection}/>
-                    ))
-                }
-            </div>
-        )
-    }
+function ShopPage({collections}) {
+    console.log(collections);
+    return (
+        <div className="shop-page container">
+            {
+                collections.map(collection => (
+                    <ShopCollection key={uuid()} {...collection}/>
+                ))
+            }
+        </div>
+    )
 }
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectShopItems
+})
+export default connect(mapStateToProps)(ShopPage)
