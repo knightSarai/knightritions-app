03d8d9c7654798fbb24583b0aeaa1eeff648f18e
knightSarai <knight.sarai.dev@gmail.com>
2020-12-05 19:00:31 +0200

    make the fetch with redux

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,53 @@
-import {UPDATE_COLLECTIONS} from './shop.types';
+import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.util';
+import collection from '../../pages/collection';
+import {
+    FETCH_COLLECTIONS_SUCCESS, 
+    FETCH_COLLECTIONS_START, 
+    FETCH_COLLECTIONS_FAILURE
+} from './shop.types';
 
-export const updateCollections = collectionMap => ({
-    type: UPDATE_COLLECTIONS,
+export const updateCollectionsStart = () => ({
+    type: FETCH_COLLECTIONS_START
+});
+
+export const updateCollectionsSuccess = collectionMap => ({
+    type: FETCH_COLLECTIONS_SUCCESS,
     payload: collectionMap
 });
+
+export const updateCollectionsFailure = message => ({
+    type: FETCH_COLLECTIONS_FAILURE,
+    payload: message
+});
+
+export const updateCollectionsAsync = () => dispatch => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(updateCollectionsStart);
+    collectionRef
+        .get()
+        .then(snapshot => {
+            const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollectionsSuccess(collectionMap);
+        })
+        .catch(err => updateCollectionsFailure(err.message))
+}
+
+/**
+    * We can use Promise pased instead of Observable patterns:
+    *  collectionRef
+    *      .get()
+            .then(snapshot => {
+               const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+               updateCollections(collectionMap);
+               setLoading(false);
+           )
+           //* but here the only time that we get data is when we render the component
+            //! this comment was in useEffect of shop page component
+            //* redux thunk don't work with Obesrvables so I have to use prommis based fetch
+            dispatch(updateCollectionsStart);
+            collectionRef.onSnapshot( snapshot => {
+                const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+                dispatch(collectionMap)
+            }, error => dispatch(updateCollectionsFailure(error.message)));
+
+*/
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,15 +1,32 @@
-import {UPDATE_COLLECTIONS} from './shop.types';
+import {
+    FETCH_COLLECTIONS_START,
+    FETCH_COLLECTIONS_SUCCESS,
+    FETCH_COLLECTIONS_FAILURE
+} from './shop.types';
 
 const INITIAL_STATE = {
-    collections: null
+    collections: null,
+    isFetching: true,
+    errorMessage: undefined
 }
 
 const shopReducer = (state=INITIAL_STATE, action) => {
     switch (action.type) {
-        case UPDATE_COLLECTIONS:
+        case FETCH_COLLECTIONS_START:
             return {
                 ...state,
+                isFetching: false
+            }
+        case FETCH_COLLECTIONS_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
                 collections: action.payload
+            }
+        case  FETCH_COLLECTIONS_FAILURE:
+            return {
+                ...state,
+                isFetching: false
             }
         default:
             return state;
